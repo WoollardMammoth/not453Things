54f1160e82d09c3f9533ea82fadc121ad1907236
Landon Woollard <woollard@csclnx02.ad.calpoly.edu>
2017-03-07 11:37:05 -0800

    First commit

---
+++ libDisk.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "libTinyFS.h"
+#include "tinyFS.h"
+
+int openDisk(char *filename, int nBytes) {
+   return 0;
+}
+
+int readBlock(int disk, int bNum, void *block) {
+   unsigned byteOffset;
+
+   char *wasteThisData
+   
+   byteOffset = bNum*BLOCKSIZE;
+
+   /*skip disk to where we want*/
+   if (-1 == lseek(disk, byteOffset, 0)) {
+      /*lseek failed*/
+      if (errno == EBADF) {
+         /*ERROR -- file descriptor is not open*/
+         return -1;
+      }
+      /*ERROR -- fd is open, but could not seek*/
+      return -2;
+   }
+   if (read(disk, block, BLOCKSIZE) != BLOCKSIZE) {
+      /*ERROR -- Could not read an entire block*/
+      return -3;
+   } else {
+      /*SUCCESS*/
+      return 0;
+   } 
+}
+
+int writeBlock(int disk, int bNum, void *block) {
+   return 0;
+}
---
+++ tinyFS.h
@@ -0,0 +1,12 @@
+#ifndef TINYFS_H
+#define TINYFS_H
+
+#define BLOCKSIZE 256
+
+#define DEFAULT_DISK_SIZE 10240
+
+#define DEFAULT_DISK_NAME “tinyFSDisk”
+
+typedef int fileDescriptor;
+
+#endif
