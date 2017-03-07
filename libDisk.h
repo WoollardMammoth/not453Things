d8b77e08b04dd22859787e11f5e5a3bbba92f6c0
Landon Woollard <woollard@csclnx02.ad.calpoly.edu>
2017-03-07 11:42:20 -0800

    First commit

---
+++ libDisk.h
@@ -0,0 +1,8 @@
+#ifndef LIBDISK_H
+#define LIBDISK_H
+int openDisk(char *filename, int nBytes);
+
+int readBlock(int disk, int bNum, void *block);
+
+int writeBlock(int disk, int bNum, void *block);
+#endif
---
+++ libTinyFS.h
@@ -0,0 +1,22 @@
+#ifndef LIBTINYFS_H
+#define LIBTINYFS_H
+
+int tfs_mkfs(char *filename, int nBytes);
+
+int tfs_mount(char *diskname);
+
+int tfs_unmount(void);
+
+fileDescriptor tfs_openFile(char *name);
+
+int tfs_closeFile(fileDescriptor FD);
+
+int tfs_writeFile(fileDescriptor FD,char *buffer, int size);
+
+int tfs_deleteFile(fileDescriptor FD);
+
+int tfs_readByte(fileDescriptor FD, char *buffer);
+
+int tfs_seek(fileDescriptor FD, int offset);
+
+#endif
