c069a7a5291403af665e9cf0e3c8e790582c6441
whipplejake <jakejwhipple@gmail.com>
2017-03-07 13:48:22 -0800

    Update libDisk.h

--- libDisk.h
+++ libDisk.h
@@ -1,5 +1,14 @@
 #ifndef LIBDISK_H
 #define LIBDISK_H
+
+typedef struct {
+    int fd;
+    void *data;
+    int nBytes;
+    char *timeStamp;
+    char *name;
+} disk;
+
 int openDisk(char *filename, int nBytes);
 
 int readBlock(int disk, int bNum, void *block);
