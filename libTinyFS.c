91cb999cf7c10e6197ad0741af1443cce4d4ddbf
Landon Woollard <woollard@calpoly.edu>
2017-03-07 12:41:48 -0800

    starting tfs_mkfs

---
+++ libTinyFS.c
@@ -0,0 +1,13 @@
+#include "tinyFS.h"
+
+/*
+ * Makes a blank TinyFS file system of size nBytes on the unix file specified
+ * by ‘filename’. This function should use the emulated disk library to open
+ * the specified unix file, and upon success, format the file to be mountable
+ * disk. This includes initializing all data to 0x00, setting magic numbers,
+ * initializing and writing the superblock and inodes, etc. Must return a
+ * specified success/error code. 
+ */
+int tfs_mkfs(char *filename, int nBytes){
+
+}
