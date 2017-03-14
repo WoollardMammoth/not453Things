a37fc8217b067afca94de932ec7fb4c710d5a60e
Jake Whipple <jakejwhipple@gmail.com>
2017-03-13 17:29:16 -0700

    This has the updated libDisk.c action with no globals

--- libDisk.c
+++ libDisk.c
@@ -9,56 +9,24 @@
 #include "tinyFS.h"
 #include "libDisk.h"
 
-disk diskArray[100];
-int totalDisks;
-
 int openDisk(char *filename, int nBytes) {
    int fd;
-   disk newDisk;
-   time_t currentTime;
 
    if((nBytes == 0) && (access(filename, F_OK) != -1)){
-      if ((fd = getFD(filename)) == -1)
-      {
-         /*not in the table but it exists*/
-         /*get fd from open*/
-         fd = open(filename, O_RDWR, 0660);
-         /*make entry in table*/
-         /*return fd*/
-         newDisk.fd = fd;
-         newDisk.name = malloc(sizeof(char) * (strlen(filename) + 1));
-         strcpy(newDisk.name, filename);
-         /*nBytes is a multiple of 256, meaning it always ends on a block boundary*/
-         newDisk.nBlocks = (nBytes - (nBytes % BLOCKSIZE)) / BLOCKSIZE;
+   		
+   		fd = open(filename, O_RDWR, 0660);
 
-         /*currentTime = time(NULL);*/
-         /*newDisk.timeStamp = ctime(&currentTime);*/
-         newDisk.timeStamp = time(NULL);
-
-         diskArray[totalDisks++] = newDisk;
-         return fd;
-      } else {
-         /*in table and exists*/
-         return fd;
-      }
-   } else if(nBytes < BLOCKSIZE) {
-      /* Failure Returned */
-      return -1;
-   } else if(-1 == (fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0660))) {
+        return fd;
+   }
+   else if(nBytes < BLOCKSIZE) {
+	   /* Failure Returned */
+    	return -1;
+   }
+   else if(-1 == (fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0660))) {
       /* Error - problems with opening the file */
       return -1;
    }
  
-   newDisk.fd = fd;
-   newDisk.name = malloc(sizeof(char) * (strlen(filename) + 1));
-   strcpy(newDisk.name, filename);
-   /*nBytes is a multiple of 256, meaning it always ends on a block boundary*/
-   newDisk.nBlocks = (nBytes - (nBytes % BLOCKSIZE)) / BLOCKSIZE;
-   
-   currentTime = time(NULL);
-   newDisk.timeStamp = ctime(&currentTime);
-   
-   diskArray[totalDisks++] = newDisk;
    return fd;
 }
 
@@ -106,16 +74,6 @@ int writeBlock(int disk, int bNum, void *block) {
    }
 
    return 0;
-}
-
-int getFD(char *filename){
-   int i;
-   for(i = 0; i < totalDisks; i++){
-      if(strcmp(filename, diskArray[i].name) == 0){
-         return diskArray[i].fd;
-      }
-   }
-   return -1;
 }
 
 
